398f5187ac0d84a1b8f76e2262a20c89039ca363
devHksingh <hksingh.dev@gmail.com>
2025-04-12 11:54:13 +0530

    add api for login

---
+++ http/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL:process.env.NEXT_PUBLIC_BACKEND_URL!,
+    headers:{
+        'Content-Type':'application/json'
+    }
+})
+
+const registerUser = async(data:{email: string; password: string; name: string; confirmPassword: string})=>{
+    console.log("BACKEND_URL",process.env.NEXT_PUBLIC_BACKEND_URL);
+    console.log("data",data);
+    
+    return api.post('/api/v1/users/register', data)
+}
+const login = async (data: { email: string; password: string }) => {
+    return api.post('/api/v1/users/login', data)
+}
+
+export {
+    registerUser,
+    login
+}
