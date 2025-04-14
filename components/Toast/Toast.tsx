3c29caedb0c73537de85301478baebb09a95ae91
devHksingh <hksingh.dev@gmail.com>
2025-04-14 22:40:19 +0530

    add Progress bar

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -13,23 +13,71 @@ interface ToastType {
 
 const Toast = ({ toast }: ToastType) => {
   const [isPaused, setIsPaused] = useState(false);
-  const timeRef = useRef<NodeJS.Timeout | null>(null);
+  const [progress, setProgress] = useState(100);
+  //   const timeRef = useRef<NodeJS.Timeout | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const remaingTimeRef = useRef<number>(toast.duration);
+  const startTimeRef = useRef<number>(Date.now());
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (!isPaused) {
-      timeRef.current = setTimeout(() => {
-        dispatch(removeToast({ id: toast.id }));
-      }, toast.duration);
-    }
-  }, [dispatch, isPaused, toast.duration, toast.id]);
+  //   useEffect(() => {
+  //     // Clear any existing interval when component mounts or isPaused changes
+  //     if (intervalRef.current) {
+  //       clearInterval(intervalRef.current);
+  //       intervalRef.current = null;
+  //     }
+  //     if (!isPaused) {
+  //       //
+  //       startTimeRef.current = Date.now();
+
+  //       const interval = 10;
+  //       intervalRef.current = setInterval(() => {
+  //         const elapsedTime = Date.now() - startTimeRef.current;
+  //         const timeUsed = Math.min(elapsedTime, remaingTimeRef.current);
+  //         // updating remaing time
+  //         remaingTimeRef.current = remaingTimeRef.current - timeUsed;
+  //         // cal progress based on remaining time
+  //         const newProgress = Math.max(
+  //           0,
+  //           (remaingTimeRef.current / toast.duration) * 100
+  //         );
+  //         setProgress(newProgress);
+  //         // remove toast on timer complete
+  //         if (newProgress <= 0) {
+  //           if (intervalRef.current) clearInterval(intervalRef.current);
+  //           dispatch(removeToast({ id: toast.id }));
+  //         }
+  //       }, interval);
+  //     } else {
+  //       // when paused cal and store the remaing time
+  //       const elapsedTime = Date.now() - startTimeRef.current;
+  //       remaingTimeRef.current = Math.max(
+  //         0,
+  //         remaingTimeRef.current - elapsedTime
+  //       );
+  //     }
+  //     // on unmount
+  //     return ()=>{
+  //         if(intervalRef.current){
+  //             clearInterval(intervalRef.current)
+  //         }
+  //     }
+  //   }, [dispatch, isPaused, toast.duration, toast.id]);
+
+  //   useEffect(() => {
+  //     if (!isPaused) {
+  //       timeRef.current = setTimeout(() => {
+  //         dispatch(removeToast({ id: toast.id }));
+  //       }, toast.duration);
+  //     }
+  //   }, [dispatch, isPaused, toast.duration, toast.id]);
 
   const handleClose = () => {
     dispatch(removeToast({ id: toast.id }));
   };
 
   const getToastClasses = () => {
-    const baseClasses = `rounded-md p-4 mb-3 shadow-lg flex items-start w-full max-w-sm`;
+    const baseClasses = `relative rounded-md p-4 mb-3 shadow-lg flex items-start w-full max-w-sm`;
     switch (toast.type) {
       case "success":
         return `${baseClasses} bg-green-50 border-l-4 border-green-500`;
@@ -54,6 +102,60 @@ const Toast = ({ toast }: ToastType) => {
         return <AlertCircle className="h-5 w-5 text-gray-500" />;
     }
   };
+  const getprogressBar = () => {
+    switch (toast.type) {
+      case "success":
+        return `bg-green-400 absolute bottom-0 left-0 h-1 transition-all duration-300 ease-linear `;
+      case "error":
+        return `bg-red-400 absolute bottom-0 left-0 h-1 transition-all duration-300 ease-linear`;
+      case "info":
+        return `bg-blue-400 absolute bottom-0 left-0 h-1 transition-all duration-300 ease-linear`;
+      case "default":
+        return `bg-gray-400 absolute bottom-0 left-0 h-1 transition-all duration-300 ease-linear`;
+    }
+  };
+
+  useEffect(() => {
+    startTimeRef.current = Date.now();
+    // remaingTimeRef.current = toast.duration;
+
+    const startProgress = () => {
+      const interval = 10; // Update progress every 10ms
+      intervalRef.current = setInterval(() => {
+        const elapsedTime = Date.now() - startTimeRef.current;
+        // Calculate how much time has passed since last start/resume
+        const timeUsed = Math.min(elapsedTime, remaingTimeRef.current);
+        // Update remaining time
+        remaingTimeRef.current = remaingTimeRef.current - timeUsed;
+        const newProgress = Math.max(
+          0,
+          100 - (elapsedTime / toast.duration) * 100
+        );
+
+        setProgress(newProgress);
+        //
+        if (newProgress <= 0) {
+          if (intervalRef.current) clearInterval(intervalRef.current);
+          dispatch(removeToast({ id: toast.id }));
+        }
+      }, interval);
+    };
+    if (!isPaused) {
+      startProgress();
+    } else {
+      // when paused cal and store the remaing time
+      const elapsedTime = Date.now() - startTimeRef.current;
+      remaingTimeRef.current = Math.max(
+        0,
+        remaingTimeRef.current - elapsedTime
+      );
+    }
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [dispatch, isPaused, toast.duration, toast.id]);
 
   return (
     <div
@@ -62,6 +164,8 @@ const Toast = ({ toast }: ToastType) => {
       onMouseLeave={() => setIsPaused(false)}
       role="alert"
     >
+      {/* Progress bar */}
+      <div className={getprogressBar()} style={{ width: `${progress}%` }}></div>
       <div className="flex-shrink-0 mr-3">{getIcon()}</div>
       <div className="flex-1">
         <div className="flex justify-between items-start">
