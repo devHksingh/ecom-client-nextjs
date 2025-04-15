bebe011348852a7f55badf00c3c22197b78ae516
devHksingh <hksingh.dev@gmail.com>
2025-04-15 22:16:27 +0530

    formate

--- app/QueryProvider.tsx
+++ app/QueryProvider.tsx
@@ -1,12 +1,12 @@
-'use client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React, { useState } from 'react'
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useState } from "react";
 
-const QueryProvider = ({children}:{children:React.ReactNode}) => {
-    const [queryClient] = useState(()=> new QueryClient)
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+  );
+};
 
-export default QueryProvider
+export default QueryProvider;
