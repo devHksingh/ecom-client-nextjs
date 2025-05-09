db3e47f0f27257af5dfc1ecec0607685a9071085
devHksingh <hksingh.dev@gmail.com>
2025-05-09 18:19:54 +0530

    add faq section and remove not necceasry code

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,14 @@ import Carousel from "@/components/Carousel";
 import FeaturesSection from "@/components/FeaturesSection1";
 import ListOfProduct from "@/components/ListOfProduct";
 import PromoSection from "@/components/PromoSection";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import useAuth from "@/hook/useAuth";
-import useToast from "@/hook/useToast";
+
 import { homePageProduct, userCustomizeProduct } from "@/http/api";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { updateAccessToken } from "@/lib/store/features/user/authSlice";
@@ -21,9 +27,9 @@ export default function Home() {
   const userState = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
   useAuth();
-  const toast = useToast();
+  
   // isLoading, isError, error
-  const { data,  } = useQuery({
+  const { data } = useQuery({
     queryKey: ["homePageProducts"],
     queryFn: homePageProduct,
     staleTime: 40 * 60 * 1000,
@@ -137,23 +143,7 @@ export default function Home() {
       color: "bg-green-100",
     },
   ];
-  const showSuccessToast = () => {
-    console.log("btn click");
-
-    toast.success("Success!", "Your action was completed successfully.", 5000);
-  };
-
-  const showErrorToast = () => {
-    toast.error("Error!", "Something went wrong. Please try again.", 5000);
-  };
-
-  const showInfoToast = () => {
-    toast.info("Information", "This is an informational message.", 5000);
-  };
-
-  const showDefaultToast = () => {
-    toast.default("Note", "This is a default notification.", 5000);
-  };
+  
   return (
     <div className="container bg-white">
       <div className=" mx-auto py-4">
@@ -232,107 +222,48 @@ export default function Home() {
         />
       </section>
 
-      <div className="flex flex-col space-y-4">
-        <button
-          onClick={showSuccessToast}
-          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-        >
-          Show Success Toast
-        </button>
-
-        <button
-          onClick={showErrorToast}
-          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-        >
-          Show Error Toast
-        </button>
-
-        <button
-          onClick={showInfoToast}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Show Info Toast
-        </button>
-
-        <button
-          onClick={showDefaultToast}
-          className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
-        >
-          Show Default Toast
-        </button>
-      </div>
+      {/* faq */}
+      <section className="mb-16">
+        <h2 className="text-4xl lg:text-4xl  font-manrope text-center font-bold text-gray-900 leading-[3.25rem] mb-6 lg:mb-12">Frequently asked questions</h2>
+        <Accordion type="single" collapsible className="w-1/2 mx-auto ">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-2xl">
+              How can I track my order status?
+            </AccordionTrigger>
+            <AccordionContent className="text-lg">
+              You can track your order by visiting the “My Orders” section in
+              nav bar.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2" >
+            <AccordionTrigger className="text-2xl">How long does shipping take?</AccordionTrigger>
+            <AccordionContent  className="text-lg">
+              Standard shipping usually takes 8 to 14 business days. Express
+              shipping options are available at checkout for faster delivery.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3" >
+            <AccordionTrigger className="text-2xl">
+              Do you offer international shipping?
+            </AccordionTrigger>
+            <AccordionContent className="text-lg">
+              Yes, we currently ship to selected countries. Shipping fees and
+              delivery times vary by destination and will be calculated at
+              checkout.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger className="text-2xl">
+              Do you support international currency?
+            </AccordionTrigger>
+            <AccordionContent className="text-lg">
+              Yes, we can support international currency but final order cost is
+              calculated on based on dollar currency.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
     </div>
   );
 }
 
-/*
-<div className="flex flex-col lg:flex-row items-center  lg:justify-evenly gap-2 ">
-          <div className="flex items-center  w-full justify-start lg:justify-center gap-4 lg:gap-6 border  px-2">
-            <CalendarSync className="text-indigo-600 " size={32} />
-            <div>
-              <h2 className="text-lg lg:text-lg font-semibold text-stone-400">
-                We’ll replace it with a new one
-              </h2>
-              <p className="text-xl lg:text-xl font-medium">
-                30-days all-inclusive warranty
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center w-full justify-start border lg:justify-center gap-4 lg:gap-6 px-2">
-            <RefreshCcw className="text-indigo-600 " size={32} />
-            <div>
-              <h2 className="text-lg lg:text-lg font-semibold text-stone-400">
-                Send it back for free
-              </h2>
-              <p className="text-lg lg:text-xl font-medium">
-                Free shipping on returns
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center w-full justify-start border lg:justify-center gap-4 lg:gap-6 px-2">
-            <Truck className="text-indigo-600 " size={32} />
-            <div>
-              <h2 className="text-lg lg:text-lg font-semibold text-stone-400">
-                The shipping is on us
-              </h2>
-              <p className="text-lg lg:text-xl font-medium">
-                Free, contactless delivery
-              </p>
-            </div>
-          </div>
-        </div>
-*/
-
-/*
-const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-<div className="min-h-screen flex items-center justify-center">
-      <button
-        onClick={openModal}
-        className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-      >
-        Open Modal
-      </button>
-
-      <Modal 
-        isOpen={isModalOpen} 
-        onClose={closeModal} 
-        title="Example Modal"
-      >
-        <p className="text-gray-600">
-          This is an example modal that closes when you click outside of it.
-          You can put any content here you want.
-        </p>
-        
-        <div className="mt-4">
-          <input
-            type="text"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-            placeholder="Example input field"
-          />
-        </div>
-      </Modal>
-    </div>
-*/
