5c0647a9993a48173b43dea074af3132bd9f3e04
devHksingh <hksingh.dev@gmail.com>
2025-05-09 12:34:56 +0530

    .

--- components/Wishlist.tsx
+++ components/Wishlist.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Wishlist = () => {
     const store = useAppStore()
-        const {wishList} = store.getState()
+    const {wishList} = store.getState()
   return (
     
     <div className='relative '>
